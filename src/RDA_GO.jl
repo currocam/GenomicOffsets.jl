bd047f650d4a9131299c4923b74b81c3c7cf1bb4
Curro Campuzano <campuzanocurro@gmail.com>
2024-05-02 10:36:21 +0200

    Typo documentation

--- src/RDA_GO.jl
+++ src/RDA_GO.jl
@@ -1,20 +1,20 @@
 using LinearAlgebra
 using StatsBase
 """
-    RDA_GO(Y::AbstractMatrix{T1}, X::AbstractMatrix{T2}, Xstar::AbstractMatrix{T2}) where {T1<:Real,T2<:Real}
+    RDA_GO(Y::AbstractMatrix{T1}, X::AbstractMatrix{T2}, Xpred::AbstractMatrix{T2}) where {T1<:Real,T2<:Real}
 
 Compute the redundancy Analysis (RDA) genomic offset. 
 
 # Arguments
 - `Y::AbstractMatrix{T1}`: NxL matrix with individuals genotypes or allele frequencies.
 - `X::AbstractMatrix{T2}`: NxP matrix with environmental data.
-- `Xstar::Matrix{T2}`: NxP matrix with the predicted / altered environmental matrix.
+- `Xpred::Matrix{T2}`: NxP matrix with the predicted / altered environmental matrix.
 # Returns
   - A Vector{Float64} of length N with the RDA genomic offset values.
 
 """
 function RDA_GO(Y::AbstractMatrix{T1}, X::AbstractMatrix{T2}, Xpred::AbstractMatrix{T2}) where {T1<:Real,T2<:Real}
-    # Add intercept to X and Xstar
+    # Add intercept to X and Xpred
     X = hcat(ones(size(X, 1)), X)
     Xpred = hcat(ones(size(Xpred, 1)), Xpred)
     # Fit linear model
@@ -22,7 +22,7 @@ function RDA_GO(Y::AbstractMatrix{T1}, X::AbstractMatrix{T2}, Xpred::AbstractMat
     # Project predicted frequencies
     loadings = eigen(cov(X * B)).vectors
     projX = (X * B) * loadings
-    projXstar = (Xpred * B) * loadings
-    return sum((projX - projXstar) .^ 2, dims=2) / size(Y, 2)
+    projXpred = (Xpred * B) * loadings
+    return sum((projX - projXpred) .^ 2, dims=2) / size(Y, 2)
   end
   
--- src/RONA.jl
+++ src/RONA.jl
@@ -1,20 +1,20 @@
 """
-    RONA(Y::AbstractMatrix{T1}, X::AbstractMatrix{T2}, Xstar::AbstractMatrix{T2}) where {T1<:Real,T2<:Real}
+    RONA(Y::AbstractMatrix{T1}, X::AbstractMatrix{T2}, Xpred::AbstractMatrix{T2}) where {T1<:Real,T2<:Real}
 
 Compute the RONA (Risk of non-adaptnesss) genomic offset. First proposed by [Rellstab et al. (2016)]( https://doi.org/10.1111/mec.13889). 
 
 # Arguments
 - `Y::AbstractMatrix{T1}`: NxL matrix with individuals genotypes or allele frequencies.
 - `X::AbstractMatrix{T2}`: NxP matrix with environmental data.
-- `Xstar::Matrix{T2}`: NxP matrix with the predicted / altered environmental matrix.
+- `Xpred::Matrix{T2}`: NxP matrix with the predicted / altered environmental matrix.
 # Returns
   - A Vector{Float64} of length N with the RONA genomic offset values.
 
 """
-function RONA(Y::AbstractMatrix{T1}, X::AbstractMatrix{T2}, Xstar::AbstractMatrix{T2}) where {T1<:Real,T2<:Real}
+function RONA(Y::AbstractMatrix{T1}, X::AbstractMatrix{T2}, Xpred::AbstractMatrix{T2}) where {T1<:Real,T2<:Real}
     X = hcat(ones(size(X, 1)), X)
-    Xstar = hcat(ones(size(Xstar, 1)), Xstar)
+    Xpred = hcat(ones(size(Xpred, 1)), Xpred)
     # Solve LSL
     B = X \ Y
-    return sum(abs.(X * B - Xstar * B), dims=2) / size(Y, 2)
+    return sum(abs.(X * B - Xpred * B), dims=2) / size(Y, 2)
 end
